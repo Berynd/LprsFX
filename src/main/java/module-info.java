f7dad4e00230edb1563c97706fb98dbd637369e2
Berynd <gabriel.berinde93@icloud.com>
2026-01-13 10:47:08 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module appli.lprsfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens appli.lprsfx to javafx.fxml;
+    exports appli.lprsfx;
+}
