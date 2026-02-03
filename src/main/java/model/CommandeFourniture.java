3ed66d12b457a459e9b17c9f0ff6f06bd2a26dc0
Reyan BABOU <BABOU_Re@lprs.local>
2026-02-03 10:09:49 +0100

    ajout modele des classes CommandeFourniture et DemandeFourniture

--- src/main/java/model/CommandeFourniture.java
+++ src/main/java/model/CommandeFourniture.java
@@ -2,10 +2,86 @@ package model;
 
 public class CommandeFourniture {
 
-    private int IdCommandeFourniture;
+    private int idCommandeFourniture;
     private String date;
     private String status;
     private String justificationRefus;
-    private int RefGestionnaire;
-    private int RefFournisseur;
+    private int refGestionnaire;
+    private int refFournisseur;
+
+
+    public CommandeFourniture() {
+
+    }
+
+
+    public CommandeFourniture(String date,String status,
+                              int refGestionnaire,int refFournisseur) {
+        this.date = date;
+        this.status = status;
+        this.refGestionnaire = refGestionnaire;
+        this.refFournisseur = refFournisseur;
+    }
+
+
+    public int getIdCommandeFourniture() {
+        return idCommandeFourniture;
+    }
+
+    public void setIdCommandeFourniture(int idCommandeFourniture) {
+        this.idCommandeFourniture = idCommandeFourniture;
+    }
+
+    public String getDate() {
+        return date;
+
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getJustificationRefus() {
+        return justificationRefus;
+    }
+
+    public void setJustificationRefus(String justificationRefus) {
+        this.justificationRefus = justificationRefus;
+    }
+
+    public int getRefGestionnaire() {
+        return refGestionnaire;
+    }
+
+    public void setRefGestionnaire(int refGestionnaire) {
+        this.refGestionnaire = refGestionnaire;
+    }
+
+    public int getRefFournisseur() {
+        return refFournisseur;
+    }
+
+    public void setRefFournisseur(int refFournisseur) {
+        this.refFournisseur = refFournisseur;
+    }
+
+    @Override
+    public String toString() {
+        return "CommandeFourniture{" +
+                "idCommandeFourniture=" + idCommandeFourniture +
+                ", date=" + date +
+                ", status='" + status + '\'' +
+                ", justificationRefus='" + justificationRefus + '\'' +
+                ", gestionnaire=" + refGestionnaire +
+                ", fournisseur=" + refFournisseur +
+                '}';
+    }
 }
--- src/main/java/model/DemandeFourniture.java
+++ src/main/java/model/DemandeFourniture.java
@@ -2,10 +2,71 @@ package model;
 
 public class DemandeFourniture {
 
-    private int IdDemandeFourniture;
+    private int idDemandeFourniture;
     private String date;
-    private String statut;
+    private  String statut;
     private String raison;
     private String justificationRefus;
-    private int RefProfesseur;
+    private int refProfesseur;
+
+    public DemandeFourniture() {
+    }
+
+
+    public DemandeFourniture(String date, String statut,
+                             String raison, int refProfesseur) {
+        this.date = date;
+        this.statut = statut;
+        this.raison = raison;
+        this.refProfesseur = refProfesseur;
+    }
+
+    public int getIdDemandeFourniture() {
+        return idDemandeFourniture;
+    }
+    public void setIdDemandeFourniture(int IdDemandeFourniture) {
+        this.idDemandeFourniture=IdDemandeFourniture;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String getStatut() {
+        return statut;
+    }
+    public void setStatut(String statut) {
+        this.statut=statut;
+    }
+    public String getRaison() {
+        return raison;
+    }
+    public void setRaison(String raison) {
+        this.raison=raison;
+    }
+    public String getJustificationRefus() {
+        return justificationRefus;
+    }
+    public void setJustificationRefus(String justificationRefus) {
+        this.justificationRefus=justificationRefus;
+    }
+    public int getRefProfesseur() {
+        return refProfesseur;
+    }
+    public void setRefProfesseur(int refProfesseur) {
+        this.refProfesseur=refProfesseur;
+    }
+
+    @Override
+    public String toString() {
+        return "DemandeFourniture{" +
+                "IdDemandeFourniture=" + idDemandeFourniture +
+                ", date='" + date + '\'' +
+                ", statut='" + statut + '\'' +
+                ", raison='" + raison + '\'' +
+                ", justificationRefus='" + justificationRefus + '\'' +
+                ", refProfesseur=" + refProfesseur +
+                '}';
+    }
 }
